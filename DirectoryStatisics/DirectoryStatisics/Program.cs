686404254149a2ed593acb3e3e16a73ad23c3d90
Yu Hao Luan <v-yuhaoluan@microsoft.com>
2021-11-09 16:34:59 +0800

    11.08 daily task

---
+++ DirectoryStatisics/DirectoryStatisics/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+
+namespace DirectoryStatistic
+{
+    public class FileHandling
+    {
+
+        public void PrintFileList(string filepath, string pattern)
+        {
+            foreach (string files in Directory.EnumerateFiles(filepath, pattern, SearchOption.AllDirectories))
+            {
+                Console.WriteLine(files);
+            }
+        }
+
+        public int FileCount(string filepath, string pattern)
+        {
+            int count = 0;
+            foreach (string files in Directory.EnumerateFiles(filepath, pattern, SearchOption.AllDirectories))
+            {
+                count += 1;
+            }
+            return count;
+        }
+
+        public long FileSize(string filepath, string pattern)
+        {
+            long size = 0;
+            DirectoryInfo directory = new DirectoryInfo(filepath);
+            FileInfo[] files = directory.GetFiles(pattern, SearchOption.AllDirectories);
+            foreach (FileInfo dire in files)
+            {
+                size += dire.Length;
+            }
+            return size;
+        }
+
+        public double Convert(long bytes)
+        {
+            return (bytes / 1024f) / 1024f;
+        }
+        /* 
+       public void LoadArgs(string[] args)
+       {
+           Dictionary<string, string> di = new Dictionary<string, string>();
+           for(int i = 0;i < args.Length - 1;i++)
+           {
+
+           }
+
+       }
+        */
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            Console.Write("Please enter the filepath: ");
+            string filepath = Console.ReadLine();
+            Console.Write("Please enter the target pattern: ");
+            string pattern = Console.ReadLine();
+            FileHandling fi = new FileHandling();
+            Console.WriteLine("File name pattern: " + pattern);
+            Console.WriteLine("File count: " + fi.FileCount(filepath, pattern));
+            Console.WriteLine("File list: ");
+            fi.PrintFileList(filepath, pattern);
+            long size = fi.FileSize(filepath, pattern);
+            double msize = fi.Convert(size);
+            Console.WriteLine("Total size: {0} MB ({1} bytes) ", msize, size);
+
+            watch.Stop();
+            var mSeconds = watch.ElapsedMilliseconds;
+
+            Console.WriteLine("Time cost: {0}ms", mSeconds);
+
+            Console.ReadKey();
+        }
+    }
+}
--- Test01/Test01/Program.cs
+++ Test01/Test01/Program.cs
@@ -6,12 +6,18 @@ using System.Threading.Tasks;
 
 namespace Test01
 {
+    /*
+        有一座高度是10级台阶的楼梯，从下往上走，每跨一步只能向上1级或者2级台阶。
+        要求用程序来求出一共有多少种走法。
+    解： 1. 假设只差最后一步就走到第10级台阶，这时候会出现几种情况？
+        因为每一步只可以走1或者2级台阶，所以最后一步
+     */
+    //dynamic programming, 分阶段求解决策问题的数学思想。
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world!");
-            Console.ReadKey();
+            
         }
     }
 }
